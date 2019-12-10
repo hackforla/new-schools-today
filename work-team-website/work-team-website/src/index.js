cc0c73e7c19887b5a74c66bf7773a4ed300c88f2
BananaBoss22 <superhappyjt@gmail.com>
2019-12-09 19:28:04 -0800

    Added App Bar and Button's Function
    Added a function that creates an App Bar with a menu button, title, and search bar.
    Added a function that creates 4 buttons rendered horizontally.
    Removed use for "App.js"

--- work-team-website/work-team-website/src/index.js
+++ work-team-website/work-team-website/src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import SearchAppBar from './ShowAppBar';
+import ContainedButtons from './ShowButton';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<SearchAppBar />, document.getElementById('root'));
+ReactDOM.render(<ContainedButtons />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

c3dca7c114b8f5d0322d1c0167756d591b75198d
Henrymarks1 <58492005+Henrymarks1@users.noreply.github.com>
2020-03-16 19:56:29 -0700

    json parser
    takes in json data from a json file. It turns it into a string. Than it reads said string and formats it.

--- nstAppTemplate/lib/parsejsonfromfile.dart
+++ nstAppTemplate/lib/parsejsonfromfile.dart
@@ -2,8 +2,8 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 
-Future<dynamic> getJSONfromFile(BuildContext context) async {
-    String jsonString =  await DefaultAssetBundle.of(context).loadString("assets/data/contacts.json");
+Future<dynamic> getJSONfromFile(BuildContext context, String fileName) async {
+    String jsonString =  await DefaultAssetBundle.of(context).loadString("assets/data/"+fileName +".json");
     dynamic json = jsonDecode(jsonString);
     return json;
 }
